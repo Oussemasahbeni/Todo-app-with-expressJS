eacf32b7880e53a9cb444328c55d12f5cd272fb6
Oussema Sahbeni <104161749+Oussemasahbeni@users.noreply.github.com>
2023-09-01 01:49:02 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,29 @@
+import express from "express";
+import bodyParser from "body-parser";
+
+const app = express();
+const port = 3000;
+
+let tasks = [];
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.render("index.ejs", {
+    taskArray: tasks,
+  });
+});
+
+app.post("/submit", (req, res) => {
+  tasks.push(req.body["todo-task"]);
+  req.body["todo-task"] = "";
+  res.redirect("/");
+});
+app.post("/delete", (req, res) => {
+  tasks.pop(req.body["todo-task"]);
+  req.body["todo-task"] = "";
+  res.redirect("/");
+});
+app.listen(port, () => {
+  console.log(`Server is running on ${port}`);
+});
